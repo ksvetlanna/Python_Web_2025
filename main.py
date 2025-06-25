accaeb4e7e179b2ca97615f60436e59817424d44
Swe <csvetlanna@yandex.ru>
2025-06-25 15:51:03 +0300

    function "\"

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 word1 = 'пришел'
-word2 = 'увидел'
-word3 = 'победил'
+word2 = 'увидел\n' #\n перевод строки
+word3 = '\tпобедил' #\t табуляция
+word4 = '27\xB0C' #ACSII символ градуса
+print(word1,word2,word3, sep=', ', end='->')
+print(word4, end='')
