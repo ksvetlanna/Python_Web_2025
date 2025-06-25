27850ffc9820124cf6ddf3e7bb6eef9e32c8d1d0
Swe <csvetlanna@yandex.ru>
2025-06-25 16:16:33 +0300

    function "\" 2

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-# \ Начало escape sequence ("экран")
-word1 = 'пришел'
-word2 = 'увидел\n' #\n перевод строки
-word3 = '\tпобедил' #\t табуляция
-word4 = '27\xB0C' #ACSII символ градуса
-print(word1,word2,word3, sep=', ', end='->')
-print(word4, end='')
+# Формат вывода 2
+
+name = 'Игорь'
+email = 'igor@mail.ru'
+age = 32
+weight=92.93
+
+#1 способ (метод плейсхолдеры)
+# %s-string
+# %d-string
+# %f-float
+print('Имя: %s, E-mail: %s, Возраст: %d' % (name, email, age))
+
+
+#2 способ
+print('Имя: {}, E-mail: {}, Возраст: {}' .format (name, email, age))
+
+#3 способ есть только с версии python 3.6
+print(f'Имя: {name}, E-mail: {email}, Возраст: {age}, Вес: {weight:.2f}') #2f-количество знаков после запятой
