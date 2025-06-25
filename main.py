96e9a15ce005d9b564fd7b3f2c9dcef795dc340a
Swe <csvetlanna@yandex.ru>
2025-06-25 15:16:09 +0300

    Len function

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-'''a=123456
-
-length=len(str(a))
-print(length)'''
-
-word=input('введите слово :')
-if not (word) or len(word)<4:
-    print('Вы ничего не ввели или слово слишком короткое')
-if len(word)>3:
-    print('Длина слова "'+ word+'" =', len(word), 'знаков')
+word1 = 'пришел'
+word2 = 'увидел'
+word3 = 'победил'
