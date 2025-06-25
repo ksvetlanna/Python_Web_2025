218d9a6aae5d2476ccf60e029929dc848df8531a
Swe <csvetlanna@yandex.ru>
2025-06-25 15:04:12 +0300

    Len function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-'''перенести значение переменных из a в b и наоборот'''
-a=3
-b=5
-a,b=b,a
-print('После:')
-print('a=',a,'b=',b)
+'''a=123456
+
+length=len(str(a))
+print(length)'''
+
+word=input('введите слово :')
+if not (word) or len(word)<4:
+    print('Вы ничего не ввели или слово слишком короткое')
+if len(word)>3:
+    print('Длина слова "'+ word+'" =', len(word), 'знаков')
