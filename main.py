b231292bd40b42d3e86bcbe35765366a593ef25a
Swe <csvetlanna@yandex.ru>
2025-06-26 16:20:01 +0300

    Nested Loops

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-# Вложенные циклы
+#задача посчитать сколько человек пришло, выбрать сколько подходит по росту от 150 до 180 и макс и мин определить
+total = 0
+total_success = 0
+total_unsuccess = 0
+min_val = float('inf')
+max_val = float('-inf')
 
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f'{i}*{j}={i*j}', end='\t')
-    print()
+while (num := int(input('Введите рост:'))) !=-1:
+    if 150 <= num <= 180:
+        total_success += 1
+        if min_val > num:
+            min_val=num
+        if num > max_val :
+            max_val = num
+    total +=1
+print(f'Число кандидатов: {total}')
+print(f'Число прошедших отбор: {total}')
+print(f'Минимальный рост: {min_val}')
+print(f'Максимальный рост: {max_val}')
