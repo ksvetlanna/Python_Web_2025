d478501ea47d803d168e9490c5dbb6c55d5aeed1
Swe <csvetlanna@yandex.ru>
2025-06-26 14:36:12 +0300

    Select Number

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-# match - case (3.10>)
-print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо\n\tQ -выход')
+counter = 1
 
-while True:
-    ch = input('Ваш выбор: ')
-    match ch:
-        case 'L'|'l'|'д'|'Д':
-            print('Свернули налево')
-        case 'R'|'r'|'к'|'К':
-            print('Свернули направо')
-        case 'F'|'f'|'а'|'А':
-            print('Пошли прямо')
-        case 'Q' | 'q' | 'й' | 'Й':
-            print('До свидания')
-            break
-        case _: #default
-            print('Выбор не ясен')
+ while counter <= 100:
+     if counter % 10 == 3:
+         print(counter, end=',')
+     counter += 1
