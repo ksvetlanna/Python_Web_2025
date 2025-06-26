88125e51bffde6bc8c093c1c333c3062d07da37a
Swe <csvetlanna@yandex.ru>
2025-06-26 15:38:52 +0300

    Max Min Sum AVG

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ N = 5
 total = 0
 min_val=float('inf') # +бесконечность
 max_val=float('-inf') # +бесконечность
+prod=1
 
 for _ in range(N):
     num=int(input('Введите целое число: '))
     if num < min_val:
         min_val=num
-    if num < max_val:
+    if num > max_val:
         max_val = num
     total += num
+    prod *= num
     average = total / N
 print(f'Сумма: {total}')
+print(f'Произведение: {prod}')
 print(f'Ср. арифметическое: {average}')
 print(f'Минимум: {min_val}')
 print(f'Максимум: {max_val}')
