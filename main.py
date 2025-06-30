37381577895866d46f38ffbd509a88537d23bc5d
Swe <csvetlanna@yandex.ru>
2025-06-30 14:06:23 +0300

    list info5

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 # удаление
 # 1пример
-lst = list(range(10))
-del lst[2]
-print(lst)
-
-# 2пример
-lst = list(range(10))
-del lst[::2] #удалить каждый 2ой элемент
-print(lst)
-
-# 3пример
-lst3 = list(range(10))
-lst3.pop(5) # удалит значение по индексу 5
-print(lst3)
-
-
-lst4 = [1,7,3,5,4,5,6,4,2]
-lst4.sort() # сортирует
-print(lst4)
+a = ['a','b','c']
+b = a.copy()   # или  a[:] полный срез от а записать в b
+b.append('d')
+print(id(a))
+print(id(b))
+print(a)
+print(b)
