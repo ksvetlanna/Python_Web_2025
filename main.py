4ac67c066def2b71cd945c823ca77c8f113d5440
Swe <csvetlanna@yandex.ru>
2025-06-30 16:03:50 +0300

    Sorted function

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# Задача. Ввести список студентов и их средний балл
-N = 3
-students = []
-
-# упаковали корт еж в список
-for _ in range(N):
-    student, avarage = input('ФИО: '), float(input('Ср. балл:'))
-    students.append((student, avarage))
-print(students)
-
-for st in students:
-    student, avarage = st
-    print('Студент: ',student)
-    print('Средний балл: ', avarage)
+# Функция sorted()
+s = {'Иванов', 'Петров', 'Сидовор'} # множество
+r = True # False
+lst = sorted(s, reverse=r)
+#lst = sorted(s) 1ый способ
+#lst = list(s) 2ой способ тоже самое что и сточка выше
+#lst.sort() 2ой способ тоже самое что и сточка выше
+print(*lst, sep=', ')
