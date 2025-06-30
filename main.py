fc8b0e02d0df28518cd89c0fdfd26c52b1d86749
Swe <csvetlanna@yandex.ru>
2025-06-30 13:03:06 +0300

    list info4

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+# удаление
 # 1пример
 lst = list(range(10))
-for item in lst:
-    print(item, '-', item **2)
-
-
-
+del lst[2]
+print(lst)
 
 # 2пример
 lst = list(range(10))
-slice = lst[1:len(lst):2]
-print(slice)
-for item in range(0,len(lst),1):
-    print(lst[item], '-', lst[item] **2)
+del lst[::2] #удалить каждый 2ой элемент
+print(lst)
+
+# 3пример
+lst3 = list(range(10))
+lst3.pop(5) # удалит значение по индексу 5
+print(lst3)
+
+
+lst4 = [1,7,3,5,4,5,6,4,2]
+lst4.sort() # сортирует
+print(lst4)
