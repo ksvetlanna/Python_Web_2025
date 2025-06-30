901f99474c035c8c985f9dff15996009882fd229
Swe <csvetlanna@yandex.ru>
2025-06-30 11:12:56 +0300

    Find and it's overloading

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 # НАЧАЛО И ОКОНЧАНИЕ СТРОКИ
-# startswith и endswith
+# 1.find('подстрока')
+# 2.find('подстрока', start) - с какого места искать (считает от 0)
+# 3.find('подстрока', start, end) - с какого места искать и по какое
 
-s = 'смотреть'
 
-if s.lower.().startswith('смо'):
-    print('да')
+s = 'синхрофазотрон'
+ch='о'
 
-if s.endswith('еть'):
-    print('да')
+
+# по умолчанию find ищет первое вхождение, если значение не найдено то выведет -1
+'''index = s.find('еть') # с начала строки s
+print(index)
+
+index = s.find('еть', 10) # ищет с позиции start
+print(index)
+
+index = s.find('ер', 10, 15) # ищет в промежутке с позиции start по end
+print(index)'''
+
+if ch in s:
+    count=s.count(ch)
+    print(f'Буквы {ch} встречается в слове {s} {count} раз.')
+    print('Её позиция/позиции:', end=' ')
+    start=0
+    for i in range(count):
+        pos = s.find(ch, start)
+        start = pos + 1
+        print(pos, end=' ')
+else:
+    print(f'Буквы {ch} нет в слове "{s}".')
+
