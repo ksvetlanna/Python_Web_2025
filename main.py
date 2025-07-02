656ae00034959a8180411ffcb68ed7b5a6961b9d
Swe <csvetlanna@yandex.ru>
2025-07-02 15:01:37 +0300

    Args and Name

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 # Функция, с переменным числом аргументов
-# когда во входном параметре указана * то выведет кортеж
-def multy(first, *args): # first позиционный аргумент, *args- переменное кол-во элементов
-    #print(len(args)) # подсчет числа аргументов
-    #print(args) # по индексу, либо перебором в цикле
-    if not args:
-        return first
-    result = first
-    for arg in args:
-        result *= arg
-    return result
+def sandwich(type_of_meal, with_onion=False, with_tomato=False):
+    print('Булочка')
+    if with_onion:
+        print('Лук')
+    print(type_of_meal)
+    if with_tomato:
+        print('Помидоры')
+    print('Булочка')
 
-def calc(*args: tuple, operator: str='+') -> any:
-    match operator:
-        case '+':
-            result = 0
-            for i in args:
-                result +=1
-        case '*':
-            result = 1
-            for i in args:
-                result *= 1
-        case _:   # случай по дефолту
-            return -float('inf')
-    return result
-
-
-def fio(name, surname):
-    return f'{name} {surname}'
-
-print(fio(name='Остап', surname='Бендер'))
-print(multy(2,3,4,5))
-print(calc(1,2,3, operator='*'))
+sandwich('Котлета', with_onion=True)
