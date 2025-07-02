99ee8d79e3b8f64469fc7ec704629e503409bcbb
Swe <csvetlanna@yandex.ru>
2025-07-02 14:52:21 +0300

    Args2

--- main.py
+++ main.py
@@ -10,9 +10,24 @@ def multy(first, *args): # first позиционный аргумент, *args-
         result *= arg
     return result
 
+def calc(*args: tuple, operator: str='+') -> any:
+    match operator:
+        case '+':
+            result = 0
+            for i in args:
+                result +=1
+        case '*':
+            result = 1
+            for i in args:
+                result *= 1
+        case _:   # случай по дефолту
+            return -float('inf')
+    return result
+
 
 def fio(name, surname):
     return f'{name} {surname}'
 
 print(fio(name='Остап', surname='Бендер'))
 print(multy(2,3,4,5))
+print(calc(1,2,3, operator='*'))
