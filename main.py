bf593d4c611424010c66c57219f4d76478ca0c2e
Swe <csvetlanna@yandex.ru>
2025-07-02 11:23:32 +0300

    Local Global and main

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 # Области видимости
-square = 'Дворцовая площадь'
+def greet(name):
+    print('Привет', name)
+    name = 'друг'
+    print('Здравствуй,', name)
 
-def square_area(length: int, width: int) -> None:
-    '''
-
-    :param Функуия вычисления площади:
-    :param width:
-    :return:
-    '''
-
-    area = length * width
-    print(f'Площадь площади "{square}" = {area}')
-
-def circle_length(radius):
-    perimetr = 2 * PI * radius
-    print(f'Длина окружности с радиусом {radius}={perimetr:.2f}') #.2f - это формат вывода, сколько после запятой
-
-def print_array(array: list) -> None:
-    for item in array:
-        print(item)
+greet('Петр')
 
 
-words = ['Привет', 'мир']
-PI = 3.14 # переменную можно ввести перед вызовом функции
-print('Давай встретимся, на', square)
-s = square_area(320 * 240)
-print(s)
-print('Ну что? Давай встретимся, на', square)
-circle_length(5)
+def main():
+    area = 'Дворцовая площадь'
+    print('Давай встретимся, где', area)
+    sq
 
-
-
-print_array(words)
-print_array(['a','b','c'])
+main() # вызывает ВСЕ функции
