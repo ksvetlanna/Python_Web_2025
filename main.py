bfb9c6b2226a0c8425a1e07b19d8aa4e93b2739a
Swe <csvetlanna@yandex.ru>
2025-07-02 11:06:31 +0300

    Prim global

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 # Области видимости
-# Пример как делать НЕЛЬЗЯ!
-a =[1,2]
+square = 'Дворцовая площадь'
 
-def change_array():
-    a[0] = 0
-change_array()
-print(a)
+def square_area(length: int, width: int) -> None:
+    '''
+
+    :param Функуия вычисления площади:
+    :param width:
+    :return:
+    '''
+
+    area = length * width
+    print(f'Площадь площади "{square}" = {area}')
+
+def circle_length(radius):
+    perimetr = 2 * PI * radius
+    print(f'Длина окружности с радиусом {radius}={perimetr:.2f}') #.2f - это формат вывода, сколько после запятой
+
+def print_array(array: list) -> None:
+    for item in array:
+        print(item)
+
+
+words = ['Привет', 'мир']
+PI = 3.14 # переменную можно ввести перед вызовом функции
+print('Давай встретимся, на', square)
+s = square_area(320 * 240)
+print(s)
+print('Ну что? Давай встретимся, на', square)
+circle_length(5)
+
+
+
+print_array(words)
+print_array(['a','b','c'])
