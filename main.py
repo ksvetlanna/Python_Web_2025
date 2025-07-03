efd258b853dd86a7d5d03d753d0eb8f3d8f9779e
Swe <csvetlanna@yandex.ru>
2025-07-03 12:24:42 +0300

    Sorting Key

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
-#варианты сортировки
-#сортировка по длинне словаа
-#fruits = ['арбуз', 'ананас', 'банан', 'ежевика', 'малина']
-# print(sorted(fruits, key=lambda ch: len(ch)))
-
-
-ENGLISH_ABC = [chr(ch) for ch in range(ord('a'), ord('z') + 1)]
-RUSSIAN_ABC = [chr(ch) for ch in range(ord('а'), ord('я') + 1)] + ['ё']
-ABC = (set(ENGLISH_ABC) ^ set(RUSSIAN_ABC) ^
-       set([x.upper() for x in ENGLISH_ABC]) ^
-       set([x.upper() for x in RUSSIAN_ABC]))
-# print(ABC)
-# print(ENGLISH_ABC)
-# print(RUSSIAN_ABC)
-txt = 'Я знаю, что я ничего не знаю. Но другие не знают и этого. А значит, я знаю больше, чем они.'
-
-d={}
-
-
-def remove_punctuation(text):
-    return ''.join(filter(lambda x: x in ABC ^ {' '}, text))
-
-
-def get_words(text: str) -> list:
-    return remove_punctuation(text).split()
-
-
-def long_words(text, length=4) -> filter:
-    return filter(lambda word: len(word) >= length, get_words(text))
-
-
-words = get_words(txt.lower())
-print(words)
-
-# считаем частоту слов
-for word in words:
-    if word in d:
-        d[word] += 1
-    else:
-        d[word] = 1
-
-# reverse сортировать по количеству
-res = {k: v for k,v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
-
-for k,v in d.items():
-   print(k, v)
-
 # ключ сортировки
+
+fruits = [ 'ананас', 'банан', 'ежевика', 'арбуз', 'малина']
+
+#sort сортирует список наместе и при выводе в print выводит None
+# сортировка в примере сначала по 1ой букве - s[1], потом по последней s[-1]
+print(sorted(fruits, key=lambda s: (s[1], s[-1])))
+#------------------------------------------------------------------------------------------------------------
+# сортировка список списка
+goods = [
+    ['Утюг', 1500.00, 2],
+    ['Фен', 1000.00, 5],
+    ['Телевизор', 8000.00, 3]
+]
+
+# сортировка по цене, количеству и названию если нужно сортировать в обратно порядке то использовать revers
+print(sorted(goods, key=lambda s: (s[1], s[2],s[0]) ))
