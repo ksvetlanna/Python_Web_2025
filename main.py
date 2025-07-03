f6effe940696ecf1a0ed304628cb9eb5e3ca5190
Swe <csvetlanna@yandex.ru>
2025-07-03 11:36:54 +0300

    dict comprehensive

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 # Анонимные функции (однострочники, безымянные)
 # lambda - функции
 # lambda <аргументы>: <выражение>
+# словарные выражения
 
-# сформировать список английского алфавита
-english_abc = set([chr(ch) for ch in range(ord('a'), ord('z') +1)])
-russian_abc = set([chr(ch) for ch in range(ord('а'), ord('я') +1)]+['ё'])
+# создать словарь четных чисел от 1 до 10
+range(2,10)
+squares = {n: n**2 for n in range(2,10) if n%2 ==0}
+print(squares)
 
-# ^ только уникальные элементы множеств english_abc и russian_abc
-ABC = english_abc ^ russian_abc
+source_dict = {
+    'x': 1,
+    'y': 2,
+    'z': 3,
+}
+dest_dict = {k: v * 2 for k, v in source_dict.items()}
+print(dest_dict)
 
-txt = 'Однажды, теперь и потом.'.lower()
-# очищает текст от лишних символов таких как (, - :)
-def remove_punctuation(text):
-    return ''. join(filter(lambda x: x in ABC ^ {' '}, text))
-
-
-
-def get_words(text: str) -> list:
-    return remove_punctuation(text).split()
-
-
-# возвращает список слов длинна которых больше или равна 4
-def long_words(text, length=4) -> filter:
-    return filter(lambda word: len(word) >= length, get_words(text))
-
-
-print(list(long_words(txt)))
