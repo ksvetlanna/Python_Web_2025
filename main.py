49c73c3b13a54ea0f00fa9eaa53cc4f4f0d326f2
Swe <csvetlanna@yandex.ru>
2025-07-03 14:39:44 +0300

    Stdin Sample

--- main.py
+++ main.py
@@ -19,10 +19,22 @@ print(all(list(map(lambda x: len(x) > 3, words))))
 
 for line in sys.stdin:
     print(line)'''
-
-
+#------------------------------------------------------------------------------------------
+'''раз два три
+елочка гори'''
+# вывести те слова где в кортеже их меньше и с сортировкой через тире
 import sys
 
 data = sys.stdin.readlines()
 data = [d.strip('\n') for d in data]
-print(data)
+
+temp = []
+# создали пустой список куда сохраним кортежи (номер строки и число строк в строке с этим индексом
+for i, s in enumerate(data): # enumerate пронумеровывает список
+    temp.append((i, len(s.split()))) # разбиваем строку на слова
+temp.sort(key=lambda x:x[1]) # сортируем по 2му элементу (меняем кортежи местами)
+#[(1, 2), (0, 3)]
+index = temp[0][0]    #
+res = sorted(data[index].split()) # разбиваем на слова и выводим через -
+print(*res, sep='-')
+
