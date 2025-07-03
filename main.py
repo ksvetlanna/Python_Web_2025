a41f90425ffc9cd1b12b763df21cd25b3f19aa3f
Swe <csvetlanna@yandex.ru>
2025-07-03 15:17:19 +0300

    Recursion Factorial

--- main.py
+++ main.py
@@ -1,40 +1,20 @@
-# проверка коллекций: any(), all()
-# any - любой элемент коллекции вернул True
-# all - все элементы коллекции вернули True
-import sys
+# Рекурсия - функция вызывает сама себя
+'''def factorial(count): # если факториал 5 = 1 * 2 * 3 * 4 * 5 = 120
+    result = 1
+    for i in range(2, count +1):
+        result *= i
+    return result
 
-print(all([1,2,3])) # все элементы не нулевые
+for x in range(10):
+    print(x, factorial(x))'''
 
-print(all([1,2,0])) # один элемент нулевой
+#-------------------------------------------------------------------------------------
+#функция вызывает сама себя и первое, что прописываем это условие выхода из функции
+def factorial(x):
+    if x == 1 or x == 0:      # базовый вариант
+        return 1
+    return x * factorial(x-1) # рекурсивная пружина
 
-print(all([]))
 
-words = 'один два три'.split() # >3
-#list_for_analize = list(map(lambda x: len(x) > 3, words))
-print(all(list(map(lambda x: len(x) > 3, words))))
-
-#------------------------------------------------------------------------------------------
-# потоковый ввод sys.stdin (Ctrl + D)
-'''import sys
-
-for line in sys.stdin:
-    print(line)'''
-#------------------------------------------------------------------------------------------
-'''раз два три
-елочка гори'''
-# вывести те слова где в кортеже их меньше и с сортировкой через тире
-import sys
-
-data = sys.stdin.readlines()
-data = [d.strip('\n') for d in data]
-
-temp = []
-# создали пустой список куда сохраним кортежи (номер строки и число строк в строке с этим индексом
-for i, s in enumerate(data): # enumerate пронумеровывает список
-    temp.append((i, len(s.split()))) # разбиваем строку на слова
-temp.sort(key=lambda x:x[1]) # сортируем по 2му элементу (меняем кортежи местами)
-#[(1, 2), (0, 3)]
-index = temp[0][0]    #
-res = sorted(data[index].split()) # разбиваем на слова и выводим через -
-print(*res, sep='-')
-
+for x in range(10):
+    print(x, factorial(x))
