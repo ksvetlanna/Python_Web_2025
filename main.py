d4bfe29e10b0511198a359388a99b611fe86b810
Swe <csvetlanna@yandex.ru>
2025-07-04 12:55:46 +0300

    modul pprint

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
 # Встроенные библиотеки
-# datetime работает с датой  и временем
-'''import datetime as dt
+# pprint
 
-print(dt.datetime.now())
-print(dt.datetime.now().date())
-print(dt.datetime.now().time())
 
-time = dt.datetime.now()
+from pprint import pprint
 
-fdat = time.strftime('%d/%m/%y') # маска для вывода d-день m-месяц y-год
 
-print('Сегодня: ', fdat)
+matrix =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
 
-ftime = time.strftime('%H:%M:%S') # маска для вывода H-часы M-минуты S-секунды
-
-print('Время: ', ftime)'''
-#----------------------------------------------------------------------------
-
-import datetime as dt
-my_time = dt.time(15,27,32)
-print(my_time) #выводит объект который потом нужно преобразовать в строку
-my_dat = dt.date(2025,7,4)
-print(my_dat)
-
-my_dat_time = dt.datetime.combine(my_dat, my_time) # combine - соединяет (комбинирует)
-print(my_dat_time)
-
-date1 = dt.date(2025,6,15)
-date2 = dt.date(2025,7,3)
-dalta = date2-date1
-
-print(dalta)
-#----------------------------------------------------------------------------
+print(matrix)
