34c7d99650ec051f2482a6b611c4007b7b600035
Swe <csvetlanna@yandex.ru>
2025-07-07 15:52:35 +0300

    Files: with ... as

--- main.py
+++ main.py
@@ -6,24 +6,8 @@
 # a - append (запись в конец)
 # r - read - чтение (по умолчанию)
 
-# построчное чтение №1
-'''fo = open('info.txt', 'rt')
-while text := fo.readline():
-    print(text.rsplit('\n'))
-fo.close()'''
-#-----------------------------------------------------------------------------------------------------------
-
-# построчное чтение №2
-'''fo = open('info.txt', 'rt')
-lst = fo.readlines()
-lst = list(map(lambda  x: x.strip('\n'), lst))
-print(lst)
-fo.close()
-'''
-# построчное чтение №3
-#-----------------------------------------------------------------------------------------------------------
-fo = open('info.txt', 'rt')
-text = fo.read()
-lst = text.splitlines()
-print(lst)
-fo.close()
+# Открытие с менеджером контеста
+with open('info.txt', 'rt') as fo:
+    text = fo.read()
+    lst = text.splitlines()
+    print(lst)
