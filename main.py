3a7608f6cd224cebfe22495de244219a1856bea0
Swe <csvetlanna@yandex.ru>
2025-07-07 12:27:01 +0300

    xlsx Template

--- main.py
+++ main.py
@@ -14,20 +14,27 @@ ws.title = 'Отчет'
 
 wb.save('Docs/report.xlsx')'''
 
-
-from itertools import count
-
 #Запись данных в существующий файл
 from openpyxl import load_workbook
 
-#открываем(загружаем) рабочую книгу
 wb = load_workbook('Docs/report.xlsx')
 
-#активный лист
 ws = wb.active
 
-#способы записи
-ws['F1'] = 'Привет мир'    #в какую ячейку писать (латинские буквы)
-ws.cell(row=1, column=3, value='Hello!')
+#Заголовки
+ws['A1'] = 'ФИО'
+ws['B1'] = 'Должность'
+ws['C1'] = 'Отдел'
 
-wb.save('Docs/newtable.xlsx')
+#Данные
+employees = [
+    ['Ивано И.И.', 'Менеджер', 'Продажи'],
+    ['Петров И.И.', 'Бухгалтер', 'Финансы'],
+    ['Сидорова И.И.', 'Аналитик', 'ИТ',],
+]
+
+for row, data in enumerate(employees, start=2):
+    ws.cell(row=row, column=1, value=data[0])
+    ws.cell(row=row, column=2, value=data[1])
+    ws.cell(row=row, column=3, value=data[2])
+wb.save('Docs/employees.xlsx')
