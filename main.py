acf45d6b42d9bfab562c6f1304705a5642e21983
Swe <csvetlanna@yandex.ru>
2025-07-07 15:48:25 +0300

    Files: read multy string

--- main.py
+++ main.py
@@ -6,8 +6,24 @@
 # a - append (запись в конец)
 # r - read - чтение (по умолчанию)
 
+# построчное чтение №1
+'''fo = open('info.txt', 'rt')
+while text := fo.readline():
+    print(text.rsplit('\n'))
+fo.close()'''
+#-----------------------------------------------------------------------------------------------------------
 
-fo = open('info.txt', 'at', encoding='utf-8')
-#fo.write(' Хороший текст.')
-print('\n А вот это будет уже с новой строки.', file=fo)
+# построчное чтение №2
+'''fo = open('info.txt', 'rt')
+lst = fo.readlines()
+lst = list(map(lambda  x: x.strip('\n'), lst))
+print(lst)
+fo.close()
+'''
+# построчное чтение №3
+#-----------------------------------------------------------------------------------------------------------
+fo = open('info.txt', 'rt')
+text = fo.read()
+lst = text.splitlines()
+print(lst)
 fo.close()
