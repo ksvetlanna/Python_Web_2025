6ff99be824308d4d96252b059ecb302ba643aa6b
Swe <csvetlanna@yandex.ru>
2025-07-07 11:14:44 +0300

    Docx Creating

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
-'''
-from PIL import Image, ImageFilter, ImageEnhance
+# Внешние библиотеки
+# Графика
+# Word - DOCX (python-docx)
+# pip freeze > requirements.txt - создание файла зависимости
+# pip install -r requirements.txt - установка списка библиотек
 
-orig = Image.open('images/Python.jpg').convert('RGB')
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Cm, Inches, Mm, Pt # для размера см, дюймы, мм, поинт
 
-blur_image = orig.filter(ImageFilter.GaussianBlur(radius=2)) # размытие, можно делать больше или меньше
-blur_image.show()'''
-#-----------------------------------------------------------------------------------------------------
-'''from PIL import Image, ImageFilter, ImageEnhance
+doc = Document() # создание экземпляра документа
 
-orig = Image.open('images/Python.jpg').convert('RGB')
-# усиление резкости
-enchancer = ImageEnhance.Sharpness(orig)
-sharpened_image = enchancer.enhance(4.0) # четкость
-sharpened_image.show()'''
-#-----------------------------------------------------------------------------------------------------
-#контуры
-from PIL import Image, ImageFilter, ImageEnhance
+#добавили одину пустую строку
+paragraph = doc.add_paragraph()
+# Добавление заголовка
+doc.add_heading('Отчет за месяц', 1) # заголовок 1го уровня
 
-orig = Image.open('images/Python.jpg').convert('RGB')
-# Получить контуры
-edges = orig.filter(ImageFilter.FIND_EDGES)
+#Добавляем абзац
+paragraph = doc.add_paragraph('В этом отчете представлены')
 
-edges.show()
+# что-то добавляем в абзац
+paragraph.add_run(' ключевые показатели').bold = True # делаем жирным
+
+#добавили одину пустую строку
+paragraph = doc.add_paragraph()
+paragraph_format = paragraph.paragraph_format
+
+#ненумерованный
+paragraph = paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
+paragraph = doc.add_paragraph('Первый пункт', style='list Bullet')
+paragraph = doc.add_paragraph('Второй пункт', style='list Bullet')
+
+#нумерованный
+paragraph = paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
+paragraph = doc.add_paragraph('Первый пункт', style='list Number')
+paragraph = doc.add_paragraph('Второй пункт', style='list Number')
+
+
+
+#Добавляем таблицу
+table = doc.add_table(rows=3, cols=3)
+#Заполняем
+for i,row in enumerate(table.rows):
+    for j, cell in enumerate(table.columns()):
+        cell.text = f'Строка {i+1}, Столбец {j+1}'
+
+
+doc.add_paragraph()
+doc.add_picture('images/sunny_day.jpg', width=Mm(105))
+doc.save('docs/report.docx')
