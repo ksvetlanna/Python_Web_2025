d4ed1faf4845fc22d5ddf5810a9e941232ab5d11
Swe <csvetlanna@yandex.ru>
2025-07-08 12:40:17 +0300

    Exceptions: Task 1

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-# Утверждения (assertions)
-# В основном для нужд тестирования
+# Задача 1
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 try:
-    text = input('Введите текст: ')
-    assert len(text) > 3 # это утверждение
-except AssertionError:
-    print('Слишком короткий текст')
+    index = int(input('Введите индекс: '))
+    if not -len(lst) < index < len(lst) - 1:
+        raise ValueError('Индекс вне диапазона')
+    res = lst[index]
+    print(f'Число по индексу {index}: {res}')
+except ValueError as exp:
+    mess = exp.args
+    if mess[0].startswith('invalid literal'):
+        print(f'Вводить надо числа')
+    else:
+        print(exp)
