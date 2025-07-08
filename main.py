c1030f6489ea966b5e36f493f1990e794d4946b8
Swe <csvetlanna@yandex.ru>
2025-07-08 16:27:00 +0300

    Ternary If 2

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 # Регулярные выражения (поиск по паттерну)
 # Regular Expressions
+# Квантификаторы (quantity)
 # r - строка -raw-string ("сырая" строка)
+# {m} -ровно m раз
+# {m,} -ровно m раз и более
+# {,n} - не более n раз
+# {m,n} - от m до n (без пробела)
+# ? - от 0 до 1 (аналог {0,1})
+# * - от 0 до бесконечности (32767) {,0}
+# + - от 1 до беконечности (32767) {1,}
 '''
 import re
 
-pattern = r'\b\w{4}\b'   #все слова из 4х символов  (r'' - регулярное выражение )
-test_string = 'дома было холодно'
+#pattern = r'начало!\Z    # на что заканчивается
+#pattern = r'[0-5][0-9]'   #2числа идут подряд и изменяются от 0-5 и 0-9
+#pattern = r'[а-яА-Я]' #все буквы от а до я и от А до Я
+#pattern = r'\((.+?)\)' вытащить текст из скобок
+pattern = '[^ерм]' # исключение символов указанных в кавычках
+test_string = 'Время - 07:55'
 
-#<re.Match object; span=(8, 10), match='20'> span где в строке находится совпадение, match - что повторяется
-#result = re.search(pattern, test_string)
-result = re.findall(pattern, test_string) #если много совпадений
+result = re.findall(pattern, test_string) # если много совпадений
 
+print(result)'''
+'''
+import re
+
+pattern = r'\((.+?)\)'
+test_string = 'Поиск по образцу (pattern)'
+
+result = re.findall(pattern, test_string) # если много совпадений
 print(result)'''
 
 import re
 
-pattern = r'\d'   #все цифры от 0 до 9    (r'\d{3}' 3и цифры подряд)
-test_string = '1 телефон 4 112-2'
-
-result = re.findall(pattern, test_string) #если много совпадений
-# Тернарный условный оператор (Ternary If)
-print('Цифры  есть') if result else print('Цифр нет') #сначала выводится при выполнении условия а вконце если условие не выполнено
+pattern = 'Go{2,}gle'
+test_string = 'Google, Goooogle, Goooooooogle'
+result = re.findall(pattern, test_string)
+print(result)
