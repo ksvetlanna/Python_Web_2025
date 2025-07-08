a839b28cd07566c01a486f3edb251cfb7cff102e
Swe <csvetlanna@yandex.ru>
2025-07-08 11:29:51 +0300

    Exceptions Intro

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-from path_lib import *
+# Исключения (runtime)
 
-print(img_dir) #ссылка на директорию images прописана в файле path_lib.py
-print(font_dir) #ссылка на директорию fonts прописана в файле path_lib.py
+# try:
+#   что пытаемся сделать
+# except:
+#   обрабатываем исключения
+#else:
+#   если исключения не было
+#finally:
+#   выполняется в любом случае
+
+
+## FileNotFoundError: [Errno 2] No such file or directory: 'information.txt' - отсутствует файл
+flag = False # открывался ли на запись
+
+try:
+    fo = open('information.txt', 'rt', encoding='utf-8')
+    print(fo.read())
+    fo.close()
+except FileNotFoundError: # исключение см. название ошибки выше
+    fo = open('information.txt', 'wt', encoding='utf-8')
+    flag = True  # если было исключение то проставится flag
+    print('Файл не обнаружен и создан по умолчанию')
+    '''with open('information.txt','wt', encoding='utf-8') as fo:
+        fo.write('По умолчанию')'''
+
+else:
+    print('Файл открыт успешно. Читаем его и закрываем.')
+    print(fo.read())
+    fo.close()
+finally:
+    if flag: # если файл был открыт на запись (было исключение)
+        fo.write('По умолчанию')
+        fo.close()
+        print('Продолжаем работать.')
