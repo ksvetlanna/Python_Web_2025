e0065a6c4acc783ec102766d08950ee4f143ed66
Swe <csvetlanna@yandex.ru>
2025-07-08 15:01:40 +0300

    PyMorphy

--- main.py
+++ main.py
@@ -1,56 +1,19 @@
-# Практикум
-# Сохранить слова которые будем вводить
-# Обучаемый словарь
+# Библиотека pymorphy
+# устанавливем в Terminal
+# pip install pymorphy3 -сама pymorphy3
+# pip install -U pymorphy3-dicts-ru - словарь
+# pip freeze > requirements.txt
 
-import pickle
-from fnmatch import translate
+from pymorphy3 import MorphAnalyzer
 
-# минимальная версия, если файл dict.dat отсутствует
-voc = {
-    'стол': 'table',
-    'стул': 'chair'
-}
+form = MorphAnalyzer().parse('бутылка')[0]
 
-# функция для распечатки словаря
-def print_voc():
-    print('Сейчас словарь содержит: ')
-    for k, v in voc.items():
-        print(k, '—', v) #Alt + 0151 длинное тире
-
-# Загружаем словарь из файла
-try:
-    with open('dict.dat', 'rb') as dump_in:
-        voc = pickle.load(dump_in)
-except FileNotFoundError:
-    with open('dict.dat', 'wb') as dump_out:
-        pickle.dump(voc, dump_out)
-    print('Создан минимальный словарь: ')
-    print_voc()
-
-while True:
-    temp =  input('\nВведите слово для перевода или "#" для завершения: ')
-    word =  temp.strip().lower()
-    if word == '#' or word == '№':                                                        # если раскладка на русской клавиатуре
-        break
-
-    if word in voc.keys():
-        translate = voc[word]
-        print(f'Слово "{word}" переводится как "{translate}".\n')
+for btl in reversed(range(99)):
+    print(f'В холодильнике {btl + 1} {form.make_agree_with_number(btl + 1).word} пива')
+    print('Возьмём одну и выпьем')
+    if btl % 10 == 1 and btl != 11:
+        remain = 'Осталась'
     else:
-        print(f'Значение слова "{word}" отсутствует в словаре.')
-        newkey = f'А как слова "{word}" переводится.\n'
-        newkey += 'Если ничего не вводите нажмите ENTER,\n'
-        newkey += 'или введите его здесь: '
-        new_word = input(newkey)
-        if new_word != '' or len(new_word) > 2:
-            voc[word] = new_word                                             # внесли слово в словарь
-            print(f'Слово "{word}" с переводом "{new_word}" внесено в словарь')
-        else:
-            print('Ничего не введено или слишком короткое слово')
-            continue
+        remain = 'Осталось'
+    print(f'{remain} {btl} {form.make_agree_with_number(btl).word} пива.')
 
-# сохранить словарь
-with open('dict.dat', 'wb') as dump_out:
-    pickle.dump(voc, dump_out)
-
-print('До новых встреч!!!')
