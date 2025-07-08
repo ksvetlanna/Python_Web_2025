02b8bc8e6fa76fc0f285e9163b99c79c6f36303f
Swe <csvetlanna@yandex.ru>
2025-07-08 12:00:18 +0300

    Exceptions by Name

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ from babel.plural import value_node
 #finally:
 #   выполняется в любом случае
 
+#Выводить ошибку пока не введут целове число не равно 0
 print('Остаток от деления:')
-
-try:
-    value = int(input('На что делим число 10:'))
-    res = 10 % value
-    print(f'Остаток от деления на 10 {value} = {res}')
-except ZeroDivisionError:
-    print('На 0 делить нельзя!')
-except ValueError:
-    print('Надо вводить только целые числа')
-except Exception as exp:
-    print('Произошло исключение: ', exp.__class__.__name__, exp)
+loop = True # флаг
+while loop:
+    try:
+        value = int(input('На что делим число 10:'))
+        res = 10 % value
+        print(f'Остаток от деления на 10 {value} = {res}')
+    except ZeroDivisionError:
+        print('На 0 делить нельзя!')
+    except ValueError:
+        print('Надо вводить только целые числа')
+    except Exception as exp:
+        print('Произошло исключение: ', exp.__class__.__name__, exp)
+    else:
+        loop = False
