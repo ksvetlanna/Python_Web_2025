897323eeec7fc4954ba7166ac15652136fd195d0
Swe <csvetlanna@yandex.ru>
2025-07-08 12:19:43 +0300

    Exceptions assertions

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-# Исключения (runtime)
-
-# try:
-#   что пытаемся сделать
-# except:
-#   обрабатываем исключения
-#else:
-#   если исключения не было
-#finally:
-#   выполняется в любом случае
-
-# "бросаться исключениями" - raise
-max_val = 10
-min_val = 1
-
+# Утверждения (assertions)
+# В основном для нужд тестирования
 try:
-    val = int(input(f'Введите целое число от {min_val} до {max_val}: '))
-    if not min_val  < val < max_val:
-        raise ValueError('введенное число вне диапазона')
-    print(f'Введенное число: {val}, лежит в заданном диапазоне')
-except ValueError as exp:
-    print('Надо быть внимательнее:', exp)
+    text = input('Введите текст: ')
+    assert len(text) > 3 # это утверждение
+except AssertionError:
+    print('Слишком короткий текст')
