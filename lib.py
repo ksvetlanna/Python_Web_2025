820ad09f21b352d825bc8af997a40631e7436d5c
Swe <csvetlanna@yandex.ru>
2025-07-09 14:16:40 +0300

    OOP INIT

--- lib.py
+++ lib.py
@@ -1,3 +1,19 @@
+class Car:
+    def __init__(self, brand='Noname', model='Noname', color='Noname'):
+        self.brand = brand #'BMV'
+        self.model = model #'X5'
+        self.color = color #'black'
+        self.engine_on = False
+
+    def start_engine(self):
+        self.engine_on = True # self. - аналог глобальной переменной
+
+    def drive_to(self, place):
+        if self.engine_on:
+            print(f'Едем в {place}, на {self.brand} {self.model}')
+        else:
+            print('Двигатель не заведен, не едем')
+
 def summ(a, b):
     return a + b
 
