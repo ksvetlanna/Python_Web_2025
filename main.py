e2af640ba901a842abfa169fa868860a7370eb49
Swe <csvetlanna@yandex.ru>
2025-07-09 14:38:05 +0300

    OOP Getters and Setters

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 # ООП (encapsulation)
-# Методы классов и анализ предыдущих вызовов
-# Конструктор это метод
+# Геттеры и сеттеры
+from  lib import Person
 
-from lib import Car
-car = Car('BMV','X5','black') # если ставится 2-е скобки то вызывается конструктор
-#car.start_engine()
-car.engine_on = True
-car.drive_to('город')
-
-car2 = Car() # если ставится 2-е скобки то вызывается конструктор
-#car2.start_engine()
-car2.drive_to('город')
+p = Person()  #вызов конструктора
+p.set_age(7897)
+p.person_info()
