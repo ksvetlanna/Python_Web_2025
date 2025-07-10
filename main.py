4f91831d91f9bff321822aba9a760c05e1396441
Swe <csvetlanna@yandex.ru>
2025-07-10 15:08:44 +0300

    Special (Magic) Methods Call1

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-#__call__ - экземпляр класса становится вызываемым (как функция)
-# y = ax^2 + bx + c
-class SquareFunction:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+# ООП (inheritance)
+# класс, от которого наследуют: базовый, родительский или суперкласс
+# класс, который наследуется: производным, дочерним
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.name = 'прямоугольник'
 
-    def __call__(self, x):
-        return self.a * x ** 2 + self.b * x + self.c
+    def perimetr(self):
+        return 2 * (self.width + self.height)
 
-s = SquareFunction(1,2,3)
-print(s(2))
+    def area(self):
+        return self.width * self.height
+
+    def get_name(self):
+        return self.name
+
+
+class Square(Rectangle): #Square стал дочерним классом, а Rectangle базовым(суперкласс)
+    def __init__(self, side):
+        # super(). - обращение к базовому классу (Rectangle)
+        super().__init__(side, side) # вызываем __init__ из суперкласса Rectangle
+        self.name = 'квадрат'
+
+s = Square(5)
+print(s.area())
+print(s.perimetr())
+print(s.get_name())
