a3b1088474574b8848dff657fb5069bf2f07a93e
Swe <csvetlanna@yandex.ru>
2025-07-10 14:43:34 +0300

    Special (Magik) Method Call

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-#
-class MyTime:
-    def __init__(self, minutes, seconds):
-        if 0 <= minutes < 60:
-            self.minutes = minutes
-        if 0 <= seconds < 60:
-            self.seconds = seconds
+#__call__ - экземпляр класса становится вызываемым (как функция)
+# y = ax^2 + bx + c
+class SquareFunction:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
-    def __add__(self, other):
-        m = self.minutes + other.minutes
-        s = self.seconds + other.seconds
-        m += s//60
-        s = s % 60
-        m = m % 60
-        return MyTime(m, s)
+    def __call__(self, x):
+        return self.a * x ** 2 + self.b * x + self.c
 
-    def __str__(self):
-        return f"<Time {self.minutes:02}:{self.seconds:02}>"
-
-
-t1 = MyTime(13,00)
-t2 = MyTime(53,5)
-print(t1 + t2)
+s = SquareFunction(1,2,3)
+print(s(2))
