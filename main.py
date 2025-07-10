b6880ed119785919db818344b5d77a322cb9ad34
Swe <csvetlanna@yandex.ru>
2025-07-10 16:15:09 +0300

    Inheritance Hierarchy and Abstract

--- main.py
+++ main.py
@@ -1,79 +1,28 @@
-# ООП (inheritance)
-# класс, от которого наследуют: базовый, родительский или суперкласс
-# класс, который наследуется: производным, дочерним
-from math import pi
-from abc import ABC, abstractmethod
-from lib import shape_info
+# ООП Class
 
+class BankAccount:
+    def __init__(self, owner, balance=0.0):
+        self.owner = owner
+        self.balance = balance
 
-class Shape:  # Shape(object) - базовый класс
-    def info(self):
-        print(f'Класс:, {self.__class__.__name__}')
+    def get_balance(self):
+        return self._balance
 
-    @abstractmethod
-    def area(self):
-        pass
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+            print(f'Депозит пополнен на сумму {amount}.')
+        else:
+            print(f'Нельзя вносить отрицательную сумму на депозит.')
 
-    @abstractmethod
-    def perimetr(self):
-        pass
+    def withdraw(self, amount):
+        if 0 < amount < self._balance:
+            self._balance -= amount
+            print(f'С депозит снята сумма {amount}.')
+        else:
+            print(f'Не хватает средств. Овердрафт не доступен.')
 
-# Фигуры
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-        self.name = 'круг'
-
-    def perimetr(self):
-        return round(2 * pi * self.radius, 2)
-
-    def area(self):
-        return round(pi * self.radius ** 2, 2)
-
-    def get_name(self):
-        return self.name
-
-class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.name = 'прямоугольник'
-
-    def perimetr(self):
-        return 2 * (self.width + self.height)
-
-    def area(self):
-        return self.width * self.height
-
-    def get_name(self):
-        return self.name
-
-
-class Square(Rectangle): #Square стал дочерним классом, а Rectangle базовым(суперкласс)
-    def __init__(self, side):
-        # super(). - обращение к базовому классу (Rectangle)
-        super().__init__(side, side) # вызываем __init__ из суперкласса Rectangle
-        self.name = 'квадрат'
-
-class Triangle(Square):
-    def __init__(self, side):
-        super().__init__(side)
-        self.side = side
-        self.name = 'треугольник'
-
-    def area(self):
-        return round((self.side ** 2 * 3 ** 0.5)/4,2)
-
-    def perimetr(self):
-        return self.side * 3
-
-s = Square(5)
-print(s.area())
-print(s.perimetr())
-print(s.get_name())
-
-
-tr = Triangle(8)
-print(tr.area())
-print(tr.perimetr())
-print(tr.get_name())
+client1 = BankAccount('John')
+client1.deposit(500)
+client1.withdraw(400)
+print('Остаток:', client1.get_balance())
