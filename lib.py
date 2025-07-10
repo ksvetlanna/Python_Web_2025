0571831cd40d2def7a9f2544d231ecb25d45de97
Swe <csvetlanna@yandex.ru>
2025-07-10 14:10:17 +0300

    Special (Magik) Method Point

--- lib.py
+++ lib.py
@@ -1,4 +1,101 @@
+from unicodedata import unidata_version
+from math import pi
+
+class Student:
+    def __init__(self, name='Bill', univ=''):
+        # свойства (поля) класса
+        self._name = name
+        self._univercity = univ
+
+    def get_univercity(self):
+        return self._univercity
+
+
+class Employee:
+    def __init__(self, name='Bill', comp=''):
+        # свойства (поля) класса
+        self._name = name
+        self._company = comp
+
+    def get_company(self):
+        return self._company
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        self.name ='круг'
+
+    def perimetr(self):
+        return  round(2 * pi * self.radius,2)
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def get_name(self):
+        return self.name
+
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+        self.name = 'квадрат'
+
+    def perimetr(self):
+        return 4 * self.side
+
+    def area(self):
+        return self.side ** 2
+
+    def get_name(self):
+        return self.name
+
+def shape_info(shape):
+    print(f'Площадь: {shape.get_name()}а: {shape.area()}, Периметр: {shape.perimetr}')
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.name = 'прямоугольник'
+
+    def perimetr(self):
+        return  2 * (self.width + self.height)
+
+    def area(self):
+        return self.width * self.height
+
+    def get_name(self):
+        return self.name
+
+# для вывода названия
+rect, c, sqr = ['прямоугольник', 'круг','квадрат']
+
+
+def shape_info(shape: object):
+    if isinstance(shape, Circle):
+        fig = c
+    elif isinstance(shape, Rectangle):
+        fig = rect
+    elif isinstance(shape, Square):
+        fig = sqr
+    print(f'Площадь: {fig}а: {shape.area()}, Периметр: {shape.perimetr}')
+
+
 from itertools import count
+class Clicker:
+    def __init__(self):
+        self._counter = 0
+
+    def click(self):
+        self._counter += 1
+
+    def get_counter(self):
+        return self._counter
+
+    def reset(self):
+        self._counter = 0
 
 
 class Person:
@@ -30,12 +127,14 @@ class Person:
 class Car:
     counter = 0 # статичное свойство (счетчик машин)
 
+    @property
     def __init__(self, brand='Noname', model='Noname', color='Noname'):
-        self.brand = brand #'BMV'
+        self._brand = brand #'BMV'
         self.model = model #'X5'
         self.color = color #'black'
         self.engine_on = False
         Car.counter += 1
+        # setters
 
     def start_engine(self):
         self.engine_on = True # self. - аналог глобальной переменной
@@ -46,18 +145,13 @@ class Car:
         else:
             print('Двигатель не заведен, не едем')
 
+
     @staticmethod
     def get_counter():
         return Car.counter
 
 
 
-def summ(a, b):
-    return a + b
-
-
-def diff(a, b):
-    return a - b
 
 #print(__name__)
 
--- main.py
+++ main.py
@@ -1,8 +1,115 @@
-# ООП (encapsulation)
-# Статичные члены класса
+# ООП (polymorphism)
+from math import hypot
 
-from lib import Car
-car1 = Car()
-car2 = Car()
-car3 = Car()
-print('В парке машин: ', Car.get_counter())
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'<Point: ({self.x}, {self.y})>' #объект пишется в угловых скобках < ... >
+
+    def __repr__(self):
+        return f'<List of Points: ({self.x}, {self.y})>' # если сложный список обектов то __repr__
+
+    def __sub__(self, other):
+        return Point(abs(self.x - other.x), abs(self.y - other.y)) # abs абсолютная величина
+
+    def __add__(self, other):
+        return hypot(self.x - other.xЫзусшфд , self.y - other.y)
+
+p1 = Point(5,4)
+p2 = Point(10, 2)
+print(p1 - p2)
+print(p1 + p2)
+
+'''from idlelib.configdialog import is_int
+
+lst = list(range(1, 15))
+lst += ['a']
+
+class Stat:
+    def __init__(self, vals):
+        self.values = vals[:]  # получаем копию
+
+    def is_all_int(self) -> bool:
+        return all(isinstance(item, int) for item in self.values)
+
+    def get_min(self):
+        if self.is_all_int():
+            return min(self.values)
+        return None
+
+    def get_max(self):
+        if self.is_all_int():
+            return max(self.values)
+        return None
+
+    def get_aver(self):
+        if self.is_all_int():
+            return sum(self.values) / len(self.values)
+        return None
+
+
+s = Stat(lst)
+print(s.get_min())
+print(s.get_max())
+print(s.get_aver())
+'''
+'''class Selector:
+    def __init__(self, vals):
+        self.values = vals[:]  # получаем копию списочного выражения, что бы не испортить рабочий вариант
+
+    def get_odd(self):
+        return [x for x in self.values if x % 2 == 1]
+
+    def get_even(self):
+        return [x for x in self.values if x % 2 == 0]
+
+s = Selector(lst)
+print(s.get_odd())
+print(s.get_even())
+print(lst)'''
+'''from lib import Student, Employee, Person
+
+people = [
+    Person('Александр', 27),
+    Student('Дмитрий', 'ГУАП'),
+    Employee('Петр','Авангард')
+]
+
+for person in people:
+    if isinstance(person, Student):
+        print(person.get_univercity())
+    elif isinstance(person, Employee):
+        print(person.get_company())
+    else:
+        print(person.get_name())'''
+# ------------------------------------------------------------------------------------------------
+'''class Book:
+    def __init__(self, title, author):
+        self._title = title
+        self._author = author
+        
+    def get_title(self):
+        return self._title
+        
+    def get_author(self):
+        return self._author
+
+from lib import Book
+book = Book('Язык С++', 'Бьярн Страупструп')
+print(f'{book.get_title(), book.get_author()}')'''
+
+
+   #------------------------------------------------------------------------------------------------
+
+'''    
+print(1 + 2) # оператор + является полиморфным будет int
+print(1 + 2.0) # оператор + является полиморфным будет float
+print('abc' + 'def') # оператор + является полиморфным будет str
+print([1,2] + [3,4]) # список [1,2,3,4]
+
+def func(x, y):
+    return x + y
+print(func(2,3.0))'''
