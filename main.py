530ef11e60bf3392c618d9fb38370034ffeedb02
Swe <csvetlanna@yandex.ru>
2025-07-11 11:10:42 +0300

    Class Animal

--- main.py
+++ main.py
@@ -1,48 +1,15 @@
-# OOП Проектирование классов
-from abc import ABC, abstractmethod
+# Протоколы
 
-
-class Animal:
-    @abstractmethod
-    def make_sound(self):
-        pass
-
-
-class Dog(Animal):
-    def make_sound(self):
-        return "Гаф"
-
-
-class Cat(Animal):
-    def make_sound(self):
-        return "Мяу"
-
-
-class Elephant(Animal):
-    def make_sound(self):
-        return "Протрубил"
-
-
-class Zoo:
-    def __init__(self):
-        self.animals = []
-
-    def add_animal(self, animal):
-        self.animals.append(animal)
-
-    def make_all_sounds(self):
-        for animal in self.animals:
-            print(animal.make_sound())
-
-
-dog = Dog()
-cat = Cat()
-elephant = Elephant()
-
-zoo = Zoo()
-
-zoo.add_animal(dog)
-zoo.add_animal(cat)
-zoo.add_animal(elephant)
-
-zoo.make_all_sounds()
+# Transmission Control Protocol (TCP) — протокол управления передачей
+# Internet Protocol (IP) - разбивает на пакеты (IP-дейтаграммы)
+# TCP/IP
+# HTTP(S) - Hyper Text Transfer Protocol (Secured)
+# FTP - File Transfer Protocol
+# SMTP - Simple Mail Transfer Protocol
+# Хост-система
+# 1. Обязательная - IP-адрес: 195.34.32.11
+# 2. Необязательная - DNS (Domain Name System)
+# http(s)://www.yandex.рф//
+# ASCII %20, %2C
+# URL - Uniform Resource Locator
+# http(s)://домен.зона/page1/?param1=value1&param2=value2
