9f9778663bf944cfe53b4f25cac63f6276b5f70b
Swe <csvetlanna@yandex.ru>
2025-07-11 12:35:03 +0300

    Periodical Task

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# dir - пролистать содержимое каталога
-# cd images - попасть в директорию images
-# cd .. - выйти на директорию уровнем выше
-# mkdir templates - создать папку templates
-# rm templates - удалить папку templates (только пустую папку удалит)
-# rm -r templates - удалить папку templates (удалит все содержимое папки и потом саму папку)
-# con - выводит консоль
-# cp info.txt .templates/info.txt - скопировать файл
-# mv info.txt .templates/info.txt - переместить файл в другую директорию
+# CRON имя скрипта и когда он будет рассылаться
+# Переодические задачи
+#pip install schedule
+#pip freeze > requirements.txt
+
+import schedule
+import datetime
+
+i = 1 #глобальная переменная, менять нельзя!!!!!!!!!
+
+def job():
+    global i
+    print(f'Скрипт запустился {i} - раз')
+    i += 1
+    t = datetime.datetime.now()
+    print('Время:', t.strftime('%H:%M:%S'))
 
 
-import sys
-# sys.argv[1] - это аргументы самого скрипта
-print('Я', sys.argv[0], 'и мой аргумент', sys.argv[1])  # sys.argv[0] это аргументы скрипта (список)
+schedule.every(3).seconds.do(job)
+# сделать по рассписанию каждые 3 сек возвращает время
 
-if len(sys.argv) >= 2:
-    match sys.argv[1]:
-        case 'p':
-            print('Привет')
-        case 'g':
-            print('Пока')
-        case _:
-            print('Не понял')
 
+while True: #запуск
+    schedule.run_pending() #отслеживать выполнение (запускать)
