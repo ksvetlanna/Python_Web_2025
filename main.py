efaaa45f0249204f3143c76ec1f33860b454de74
Swe <csvetlanna@yandex.ru>
2025-07-14 15:30:39 +0300

    Json Read2

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# JSON - Java Script Object Notation
-# Для чтения :
-# load() - читает из файл
-# loads() - читает строковое представление
-
 import json
 
+with open('dogs.json', 'rt') as d:
+    # data = json.load(d) # Напрямую из файла
+    temp = d.read()  # читаем файл как строку
+    data = json.loads(temp)  # строковое представление JSON
 
-with open('dogs.json','rt') as d:
-   # data = json.load(d) #напрямую из файла
-   temp=d.read() # читаем как строку
-   data = json.loads(temp) # строковое представление JSON
-
-for k, v in data.items():
-    if type(v) == list:
-        print(f'{k}: {', '.join(v)}')
-    else:
-        print(f'{k}: {v}')
+for i in range(len(data)):
+    print(f'Питомец №{i + 1}:')
+    for k, v in data[i].items():
+        if type(v) == list:
+            print(f'\t{k}: {', '.join(v)}')
+        else:
+            print(f'\t{k}: {v}')
