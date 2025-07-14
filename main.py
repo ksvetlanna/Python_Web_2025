6deb91eb6026fea50d3ebc28535ba94a0aa6c1fa
Swe <csvetlanna@yandex.ru>
2025-07-14 15:22:50 +0300

    Json Read

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-# Zip (собрать в кучу все файл csv, поместить в архив и удалить)
+# JSON - Java Script Object Notation
+# Для чтения :
+# load() - читает из файл
+# loads() - читает строковое представление
 
-'''from zipfile import ZipFile
-import os
-
-#
-csv_files = [f for f in os.listdir() if f.endswith('.csv')]
-#print(csv_files)
-with ZipFile('arhive.zip', 'w') as myzip:
-    for file in csv_files:
-        myzip.write(file)
-        os.remove(file)'''
+import json
 
 
+with open('dogs.json','rt') as d:
+   # data = json.load(d) #напрямую из файла
+   temp=d.read() # читаем как строку
+   data = json.loads(temp) # строковое представление JSON
 
-#------------------------------------------------------------------------------------
-# распаковать весь архив (файл arhive.zip)
-'''from zipfile import ZipFile
-import os
-
-#files_to_extract = ['people.csv', 'file.csv'] # распаковать конкретные файлы из архива
-with ZipFile('arhive.zip', 'r') as zip_obj:
-    zip_obj.extractall()'''
-
-#------------------------------------------------------------------------------------
-
-from zipfile import ZipFile
-import os
-
-# получим список объектов находящихся в архиве arhive.zip
-with ZipFile('arhive.zip', 'r') as zip_obj:
-    print(zip_obj.namelist())
+for k, v in data.items():
+    if type(v) == list:
+        print(f'{k}: {', '.join(v)}')
+    else:
+        print(f'{k}: {v}')
