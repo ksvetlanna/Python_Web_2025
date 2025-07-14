f3cfc1d997ad3b6e633cb8999f46fb66d6aa6d27
Swe <csvetlanna@yandex.ru>
2025-07-14 14:41:03 +0300

    ZIp Files

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-# CSV-файлы (strptime) строку переводит в двату
+# Zip (собрать в кучу все файл csv, поместить в архив и удалить)
 
-import csv
+'''from zipfile import ZipFile
+import os
 
-# режим кватирования (QUOTE_NONNUMERIC)
-data = ['name', 25, 'town']
-with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC) #записать все в '' но числа в кавычки не заключать
-    writer.writerow(data)
+#
+csv_files = [f for f in os.listdir() if f.endswith('.csv')]
+#print(csv_files)
+with ZipFile('arhive.zip', 'w') as myzip:
+    for file in csv_files:
+        myzip.write(file)
+        os.remove(file)'''
+
+
+
+#------------------------------------------------------------------------------------
+# распаковать весь архив (файл arhive.zip)
+'''from zipfile import ZipFile
+import os
+
+#files_to_extract = ['people.csv', 'file.csv'] # распаковать конкретные файлы из архива
+with ZipFile('arhive.zip', 'r') as zip_obj:
+    zip_obj.extractall()'''
+
+#------------------------------------------------------------------------------------
+
+from zipfile import ZipFile
+import os
+
+# получим список объектов находящихся в архиве arhive.zip
+with ZipFile('arhive.zip', 'r') as zip_obj:
+    print(zip_obj.namelist())
