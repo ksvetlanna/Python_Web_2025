f2182649b2484def4b8d216d884d86ab402612fb
Swe <csvetlanna@yandex.ru>
2025-07-15 15:44:48 +0300

    Decorator Intro

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 # Декораторы
-
-def answer(question):
-    return 'думайте сами'
-
-def dialog():
-    def answer(question):
-        if question.lower().startswith('когда'):
-            return 'Никогда'
-        else:
-            return 'Упппс'
-
-    question = input()
-    while question != '':
-         print(answer(question))
-         question = input()
-
-dialog()
+# Выводит большими буквами сообщение
+def upper_case_print(old_func):
+    def new_func(*args, **kwargs):
+        args_up_case = [str(arg).upper() for arg in args]
+        old_func(*args_up_case, **kwargs)
+    return new_func
+new_print = upper_case_print(print)
+new_print('Привет, Пока')
