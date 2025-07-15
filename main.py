d749bfe9929747a7d10768cc52bc698bbec88989
Swe <csvetlanna@yandex.ru>
2025-07-15 15:49:59 +0300

    Decorator Intro

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 # Выводит большими буквами сообщение
 def upper_case_print(old_func):
     def new_func(*args, **kwargs):
-        args_up_case = [str(arg).upper() for arg in args]
-        old_func(*args_up_case, **kwargs)
+        case = kwargs.pop('case', None)
+        if case == 'U':
+            args = [str(arg).upper() for arg in args]
+        if case == 'L':
+            args = [str(arg).lower() for arg in args]
+        return old_func(*args, **kwargs)
     return new_func
 new_print = upper_case_print(print)
 new_print('Привет, Пока')
+new_print('Привет, Пока', case='U')
