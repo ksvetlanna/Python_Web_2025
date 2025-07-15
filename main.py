9c77cd520e2cc1670b09170896e9d1d6385089cd
Swe <csvetlanna@yandex.ru>
2025-07-15 16:12:21 +0300

    Decorator Time Measure

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 # Декораторы
+import time
+from turtledemo.penrose import start
 
-def outher():
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        finish = time.time()
+        print(f'Функция исполнялась:{finish - start:.4f} сек.')
+        return result
+    return wrapper
+
+@timeit
+def test():
+    time.sleep(0.8)
+
+test()
+#--------------------------------------------------------------------------------------------
+'''def logger(func):
+    counter = 0
+    def decorated_func(*args, **kwargs):
+        nonlocal counter
+        counter += 1
+        print(counter, '->', 'Аргументы:', args, 'Именованные аргументы:', kwargs)
+        result = func(*args, **kwargs)
+        print('____', 'Результат:', result)
+        return result
+    return decorated_func
+
+@logger
+def make_burger(meal='говядиной', onion=False, tomato=False):
+    print('Булочка')
+    if onion:
+        print('Луковые кольца')
+    print('Котлета с', meal)
+    if tomato:
+        print('Помидоры')
+    print('Булочка')
+make_burger('бараниной', onion=True)'''
+
+#--------------------------------------------------------------------------------------------
+'''def outher():
     x = 5
 
     def inner():
@@ -10,24 +51,9 @@ def outher():
     inner()
     print('New x=', x)
 
-outher()
+outher()'''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#--------------------------------------------------------------------------------------------
 # Декораторы
 # Выводит большими буквами сообщение
 '''def upper_case_print(old_func):
