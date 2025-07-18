bf6ab38a5eadedfb714ace4b2419e52a59578b7f
Swe <csvetlanna@yandex.ru>
2025-07-18 15:06:59 +0300

    Registration Forms

--- main.py
+++ main.py
@@ -35,6 +35,9 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html', title='Не найдено')
 
 @app.route('/')
 @app.route('/index')
